735c49c11307086e9d2abd3b42ba024396b482ff
YusufDodarov18 <email@github.com>
2025-12-28 21:35:15 +0500

    test 2

--- app/(private)/dashboard/products/[id]/page.tsx
+++ app/(private)/dashboard/products/[id]/page.tsx
@@ -1,23 +1,12 @@
 import { getProductById } from '@/app/api/product/route'
 import ProductForm from '@/components/ProductForm'
 
-export default async function ProductPage({ params }: { params: { id: string } }) {
-  const { id } = params;
-  let product = null;
-
-  try {
-    product = await getProductById(id);
-  } catch (err) {
-    console.error("Error fetching product:", err);
-  }
-
-  if (!product) {
-    return <div className="text-center py-10 text-red-500">Product not found</div>;
-  }
-
+export default async function page({params}:{params: {id:string}}) {
+    const {id}= await params
+    const product= await getProductById(id);
   return (
     <div>
-      <ProductForm product={product} />
+        <ProductForm product={product}/>
     </div>
   )
 }
--- app/(private)/dashboard/products/page.tsx
+++ app/(private)/dashboard/products/page.tsx
@@ -3,20 +3,10 @@ import { ProductsWithImages } from "@/app/types";
 import ProductTable from "@/components/ProductTable";
 
 export default async function DashBoardPage() {
-  let products: ProductsWithImages[] = [];
-  try {
-    products = await getProducts() || [];
-  } catch (err) {
-    console.error("Error fetching products:", err);
-  }
-
+  const products:ProductsWithImages[]=await getProducts()
   return (
     <div className="mt-30">
-      {products.length === 0 ? (
-        <div className="text-center py-10 text-gray-500">No products found</div>
-      ) : (
-        <ProductTable products={products} />
-      )}
+      <ProductTable products={products} />
     </div>
   )
 }
